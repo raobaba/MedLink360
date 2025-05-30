20eaa292d430248c1e3eb004db67f9d7307c4974
VishalPukale-GeniaTeq <vishal.pukale@geniateq.com>
2025-05-30 23:36:07 +0530

    scrollbar

--- frontend/src/ui-controls/scrollbar/index.jsx
+++ frontend/src/ui-controls/scrollbar/index.jsx
@@ -1,54 +1,49 @@
 import React from "react";
-// import './style.css';
 import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import "overlayscrollbars/overlayscrollbars.css";
 import PropTypes from "prop-types";
 
 const ScrollBar = ({
-  sbRef,
-  scrollStyle,
-  autoHeightMin,
-  autoHeightMax,
   children,
-  maxHeight,
-  maxWidth,
-  horizontal,
-  verticle,
-  autohide,
+  sbRef,
+  scrollStyle = "",
+  height = "100%",
+  width = "100%",
+  vertical = "scroll",
+  horizontal = "hidden",
+  autohide = "leave",
+  style = {},
   ...rest
 }) => {
   return (
-    <>
-      <OverlayScrollbarsComponent
-        ref={sbRef}
-        options={{
-          scrollbars: { autoHide: autohide },
-          overflowBehavior: { x: horizontal, y: verticle },
-        }}
-        style={{
-          maxHeight: maxHeight,
-          maxWidth: maxWidth,
-        }}
-        className={scrollStyle}
-        {...rest}
-      >
-        {children}
-      </OverlayScrollbarsComponent>
-    </>
+    <OverlayScrollbarsComponent
+      ref={sbRef}
+      options={{
+        scrollbars: { autoHide: autohide },
+        overflowBehavior: { x: horizontal, y: vertical },
+      }}
+      style={{ maxHeight: height, maxWidth: width, ...style }}
+      className={scrollStyle}
+      {...rest}
+    >
+      {children}
+    </OverlayScrollbarsComponent>
   );
 };
 
 ScrollBar.propTypes = {
   children: PropTypes.node.isRequired,
+  sbRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]),
   scrollStyle: PropTypes.string,
-  verticle: PropTypes.string,
-  horizontal: PropTypes.string,
-};
-ScrollBar.defaultProps = {
-  autohide: "leave",
-  verticle: "scroll",
-  horizontal: "hidden",
-  maxHeight: "100%",
-  maxWidth: "100%",
+  vertical: PropTypes.oneOf(["scroll", "hidden", "visible", "auto"]),
+  horizontal: PropTypes.oneOf(["scroll", "hidden", "visible", "auto"]),
+  autohide: PropTypes.oneOf(["leave", "scroll", "move", "never"]),
+  height: PropTypes.string,
+  width: PropTypes.string,
+  style: PropTypes.object,
 };
 
 export default ScrollBar;
